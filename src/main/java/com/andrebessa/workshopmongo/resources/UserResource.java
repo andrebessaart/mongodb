e01bc63e40d2dee6c04257f26c79ed79f60644fe
Andre Bessa <andrebessaart@gmail.com>
2018-08-22 19:13:55 -0300

    Delecao de usuario com DELETE

--- src/main/java/com/andrebessa/workshopmongo/resources/UserResource.java
+++ src/main/java/com/andrebessa/workshopmongo/resources/UserResource.java
@@ -51,5 +51,12 @@ public class UserResource {
 
 		return ResponseEntity.created(uri).build();
 	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+		
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 
 }
