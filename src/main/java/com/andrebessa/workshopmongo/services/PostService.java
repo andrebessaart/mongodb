d8cbc9be8acdf7116325f8c10683324237f1171a
Andre Bessa <andrebessaart@gmail.com>
2018-08-23 15:42:40 -0300

    Obtendo post por ID

---
+++ src/main/java/com/andrebessa/workshopmongo/services/PostService.java
@@ -0,0 +1,33 @@
+package com.andrebessa.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.andrebessa.workshopmongo.domain.Post;
+import com.andrebessa.workshopmongo.domain.User;
+import com.andrebessa.workshopmongo.dto.UserDTO;
+import com.andrebessa.workshopmongo.repository.PostRepository;
+import com.andrebessa.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public List<Post> findAll() {
+		
+		return repo.findAll();
+	}
+	
+	public Post findById(String id) {
+		
+		Optional<Post> user = repo.findById(id);
+		
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
